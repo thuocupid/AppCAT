482d316fcfd9d71b82217c57119d70a8e0f1f00a
Cupid <67965554+Cupid-T@users.noreply.github.com>
2021-11-26 14:23:37 +0300

    Login page & Reg form

--- app/src/main/java/com/example/appcat/LogInActivity.java
+++ app/src/main/java/com/example/appcat/LogInActivity.java
@@ -7,19 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_log_in);
 
-        TextView btn = findViewById(R.id.alreadyHaveAccount);
-
+        TextView btn = findViewById(R.id.textViewSignUp);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this, LogInActivity.class));
+                startActivity(new Intent(LogInActivity.this, RegisterActivity.class));
             }
         });
 
