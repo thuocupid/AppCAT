822264ca517c532a1259d2fe35c320c0ebb65418
Cupid <67965554+Cupid-T@users.noreply.github.com>
2021-11-27 07:48:07 +0300

    Connected to firebase Authentication

--- app/src/main/java/com/example/appcat/LogInActivity.java
+++ app/src/main/java/com/example/appcat/LogInActivity.java
@@ -4,15 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class LogInActivity extends AppCompatActivity {
+    Button BtnSignUp;
+    EditText UserName, password, email;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        email = findViewById(R.id.inputEmail);
+        UserName = findViewById(R.id.inputUserName);
+        password = findViewById(R.id.inputPassword);
+        Button BtnLogin = findViewById(R.id.btnLogIn);
+        mAuth = FirebaseAuth.getInstance();
+
+        //set Onclick listener for our login button
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Email = email.getText().toString().trim();
+                String Password = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(Email)){
+                    email.setError("Enter Email");
+                }
+                if (TextUtils.isEmpty(Password)){
+                    password.setError("Enter Password");
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                    email.setError("Enter Valid Email");
+                    email.requestFocus();
+                    return;
+                }
+                if (Password.length()<6){
+                    password.setError("Password must be more than 6 characters");
+                    password.requestFocus();
+                    return;
+                }
+
+                //logging in the user
+                mAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        startActivity(new Intent(LogInActivity.this, Dashboard.class));
+                    }
+                    else {
+                        Toast.makeText(LogInActivity.this, "Please check your Login credentials", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         TextView btn = findViewById(R.id.textViewSignUp);
         btn.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/appcat/RegisterActivity.java
+++ app/src/main/java/com/example/appcat/RegisterActivity.java
@@ -1,19 +1,79 @@
 package com.example.appcat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button BtnSignUp;
+    EditText UserName, password, email;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //declaring the variables
+        UserName = findViewById(R.id.inputUserName);
+        password = findViewById(R.id.inputPassword);
+        email = findViewById(R.id.inputEmail);
+        BtnSignUp = findViewById(R.id.btnRegister);
+        mAuth = FirebaseAuth.getInstance();
+
+        //set onclick listener for the register button
+        BtnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String UsrName = UserName.getText().toString().trim();
+                String Password = password.getText().toString().trim();
+                String Email = email.getText().toString().trim();
+
+                //making sure all fields are filled
+                if (TextUtils.isEmpty(UsrName)){
+                    UserName.setError("Enter Username");
+                } else if (TextUtils.isEmpty(Password)){
+                    password.setError("Enter password");
+                } else if (Password.length()<6){
+                    password.setError("Password must be more than 6 characters long");
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                    email.setError("Enter valid email address");
+                    email.requestFocus();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "You have successfully registered", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Failed to register! Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+        //set Onclick listener for text
         TextView btn = findViewById(R.id.alreadyHaveAccount);
 
         btn.setOnClickListener(new View.OnClickListener() {
